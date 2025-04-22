6a996f5424140a63f01ec77539fee51f959cb3f4
Wahmane-Hamza <hamza.wahm5@gmail.com>
2025-04-22 17:52:20 +0100

    asd

--- lexer_command.c
+++ lexer_command.c
@@ -12,55 +12,55 @@
 
 #include <minishell.h>
 
-t_token_type get_token_type(char *str)
+t_token_type	get_token_type(char *str)
 {
-    if (str == NULL || *str == '\0')
-        return TOKEN_EMPTY;
-    else if (ft_strncmp(str, "(", ft_strlen(str)) == 0)
-        return TOKEN_OPARENTHES;
-    else if (ft_strncmp(str, ")", ft_strlen(str)) == 0)
-        return TOKEN_CPARENTHES;
-    else if (ft_strncmp(str, "|", ft_strlen(str)) == 0)
-        return TOKEN_PIPE;
-    else if (ft_strncmp(str, ">", ft_strlen(str)) == 0)
-        return TOKEN_REDIR_OUT;
-    else if (ft_strncmp(str, "<", ft_strlen(str)) == 0)
-        return TOKEN_REDIR_IN;
-    else if (ft_strncmp(str, "&&", 2) == 0)
-        return TOKEN_AND;
-    else if (ft_strncmp(str, "||", ft_strlen(str)) == 0)
-        return TOKEN_OR;
-    else if (ft_strncmp(str, ">>", ft_strlen(str)) == 0)
-        return  TOKEN_REDIR_APPEND;
-    else if (ft_strncmp(str, "<<", ft_strlen(str)) == 0)
-        return  TOKEN_HEREDOC;
-    return TOKEN_WORD;
+	if (str == NULL || *str == '\0')
+		return (TOKEN_EMPTY);
+	else if (ft_strncmp(str, "(", ft_strlen(str)) == 0)
+		return (TOKEN_OPARENTHES);
+	else if (ft_strncmp(str, ")", ft_strlen(str)) == 0)
+		return (TOKEN_CPARENTHES);
+	else if (ft_strncmp(str, "|", ft_strlen(str)) == 0)
+		return (TOKEN_PIPE);
+	else if (ft_strncmp(str, ">", ft_strlen(str)) == 0)
+		return (TOKEN_REDIR_OUT);
+	else if (ft_strncmp(str, "<", ft_strlen(str)) == 0)
+		return (TOKEN_REDIR_IN);
+	else if (ft_strncmp(str, "&&", 2) == 0)
+		return (TOKEN_AND);
+	else if (ft_strncmp(str, "||", ft_strlen(str)) == 0)
+		return (TOKEN_OR);
+	else if (ft_strncmp(str, ">>", ft_strlen(str)) == 0)
+		return (TOKEN_REDIR_APPEND);
+	else if (ft_strncmp(str, "<<", ft_strlen(str)) == 0)
+		return (TOKEN_HEREDOC);
+	return (TOKEN_WORD);
 }
 
-t_token *create_token(char *value)
+t_token	*create_token(char *value)
 {
-    t_token *token;
+	t_token	*token;
 
-    if (!value)
-        return NULL;
-    token = malloc(sizeof(t_token));
-    token->value = ft_strdup(value);
-    token->type = get_token_type(value);
-    token->next = NULL; 
-    return (token);
+	if (!value)
+		return (NULL);
+	token = malloc(sizeof(t_token));
+	token->value = ft_strdup(value);
+	token->type = get_token_type(value);
+	token->next = NULL;
+	return (token);
 }
 
-void token_add_back(t_token **head, t_token *new_token)
+void	token_add_back(t_token **head, t_token *new_token)
 {
-    t_token *tmp;
+	t_token	*tmp;
 
-    if (!*head)
-    {
-        *head = new_token;
-        return ;
-    }
-    tmp = *head;
-    while (tmp->next)
-        tmp = tmp->next;
-    tmp->next = new_token;
+	if (!*head)
+	{
+		*head = new_token;
+		return ;
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new_token;
 }
