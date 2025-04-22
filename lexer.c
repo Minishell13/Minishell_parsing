4018d2a94cedb595a7d374c06ac2e8099d938490
Wahmane-Hamza <hamza.wahm5@gmail.com>
2025-04-22 12:15:06 +0100

    mini_shell start

---
+++ lexer.c
@@ -0,0 +1,162 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 16:00:23 by hwahmane          #+#    #+#             */
+/*   Updated: 2025/04/22 12:14:08 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+t_token_type get_token_type(char *str)
+{
+    if (str == NULL || *str == '\0')
+        return TOKEN_EMPTY; 
+    if (ft_strncmp(str, "|", ft_strlen(str)) == 0)
+        return TOKEN_PIPE;
+    else if (ft_strncmp(str, ">", ft_strlen(str)) == 0)
+        return TOKEN_REDIR_OUT;
+    else if (ft_strncmp(str, "<", ft_strlen(str)) == 0)
+        return TOKEN_REDIR_IN;
+    else if (ft_strncmp(str, "&&", ft_strlen(str)) == 0)
+        return TOKEN_AND;
+    else if (ft_strncmp(str, "||", ft_strlen(str)) == 0)
+        return TOKEN_OR;
+    else if (ft_strncmp(str, ">>", ft_strlen(str)) == 0)
+        return  TOKEN_REDIR_APPEND;
+    return TOKEN_COMMAND;
+}
+
+t_token *create_token(char *value)
+{
+    t_token *token;
+
+    token = malloc(sizeof(t_token));
+    token->value = ft_strdup(value);
+    token->type = get_token_type(value);
+    return (token);
+}
+
+void token_add_back(t_token **head, t_token *new_token)
+{
+    t_token *tmp;
+
+    if (!*head)
+    {
+        *head = new_token;
+        return ;
+    }
+    tmp = *head;
+    while (tmp->next)
+        tmp = tmp->next;
+    tmp->next = new_token;
+}
+
+// t_bool check_str_command(char *str)
+// {
+//     int i;
+//     int type;
+//     char **command;
+
+//     i = 0;
+//     while (str[i])
+//     {
+//         if (str[i] == '|' && str[i + 1] != '|' && str[i - 1] != '|')
+//             type = 0;
+//         if (str[i] == '&' && str[i + 1] == '&' && str[i - 1] != '&')
+//             type = 1;
+//         if (str[i] == '|' && str[i + 1] == '|' && str[i - 1] != '|')
+//             type = 2;
+//         if (str[i] == '>' && str[i + 1] == '>' && str[i - 1] != '>')
+//             type = 3;
+//         if (str[i] == '>' && str[i + 1] != '>' && str[i - 1] != '>')
+//             type = 4;
+//         if (str[i] == '<' && str[i + 1] != '<' && str[i - 1] != '<')
+//             type = 5;
+//     }
+//     command = ft_split(str, '|');
+// }
+
+// t_token *tokenize(char *line)
+// {
+//     t_token *head;
+//     char **commands;
+//     int i;
+//     int j;
+
+//     commands = ft_split(line, ' ');
+//     i = 0;
+//     head  = NULL;
+//     while (commands[i])
+//     {
+//         j = 0;
+//         while (commands[i][j])
+//         {
+//             if (commands[i][j] == '\n')
+//                 commands[i][j] = '\0';
+//             j++;
+//         }
+//         if (check_str_command(commands[i]) == false)
+//             token_add_back(&head, create_token(commands[i]));
+//         i++;
+//     }
+//     return head;
+// }
+
+int is_operator_char(char c) 
+{
+    return (c == '|' || c == '&' || c == '<' || c == '>');
+}
+
+t_token *lexer(char *line) 
+{
+    int i;
+    char op[3];
+    char *word;
+    int start;
+    t_token *head = NULL;
+
+    i = 0;
+    while (line[i]) 
+    {
+        if (line[i] == ' ' || line[i] == '\t') 
+        {
+            i++;
+            continue;
+        }
+
+        // Handle operators like &&, ||, >>
+        if (is_operator_char(line[i]))
+        {
+            op[0] = line[i];
+            if (line[i + 1] && line[i + 1] == line[i]) 
+            {
+                op[1] = line[i + 1];
+                i++;
+            }
+            else
+                op[1] = '\0';
+            token_add_back(&head, create_token(op));
+            i++;
+            if (line[i] == '\n')
+                line[i] = '\0';
+        }
+        else 
+        {
+            // Read a word until next space or operator
+            start = i;
+            while (line[i] && !is_operator_char(line[i]) && line[i] != ' ' && line[i] != '\t')
+                i++;
+            if (line[i - 1] == '\n')
+                line[i - 1] = '\0';
+            word = ft_substr(line, start, i - start);
+            token_add_back(&head, create_token(word));
+            free(word);
+        }
+    }
+    return head;
+}
